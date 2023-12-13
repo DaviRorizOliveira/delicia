19e8f6c9030327821acadd9cedd973e220d00b5d
laribrito <lary.29.ds@gmail.com>
2023-12-13 20:01:19 -0300

    feat: acaboooooooou

--- src/app/(public)/carteira/page.tsx
+++ src/app/(public)/carteira/page.tsx
@@ -1,60 +1,62 @@
+'use client'
 import styles from '@/app/page.module.css';
 import { FaUserAlt, FaCoffee, FaMoon, FaWallet } from 'react-icons/fa';
-import { BiSolidDish } from 'react-icons/bi';
 import { MdOutlineRestaurantMenu } from "react-icons/md";
-import { TbMessageExclamation } from "react-icons/tb";
+import { TbMessageExclamation } from 'react-icons/tb';
+import { FaCirclePlus } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.top}>
-        <div><h1>DELLÍCIA</h1></div>
-        <div className={styles.userIcon}>
-          <a href="#">
-            <span className={styles.userIcon}> <FaUserAlt /> </span>
-          </a>
+      <div style={{display: 'flex', justifyContent: "center"}}>
+        <div className={styles.top}>
+          <h1>DELLÍCIA</h1>
+          <div className={styles.userIcon}>
+            <a href="/aluno">
+              <span className={styles.userIcon}> <FaUserAlt /> </span>
+            </a>
+          </div>
         </div>
       </div>
 
       <div className={styles.middle}>
         <div className={styles.darkBox}>
-            <h1>Estêvão Vieira</h1>
+            <h1 style={{fontSize: '22px'}}>Estêvão Vieira</h1>
             <h3 className={styles.element}>Ciência da Computação</h3>
             <h2 className={styles.element}>202210345</h2>
         </div>
 
-        <div className={styles.box}>
-            <h3>SALDO</h3>
-            <h1 className={styles.element}>R$ 23,00</h1>
+        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '300px'}}>
+            <div className={styles.box} style={{width: '200px', margin: '0'}}>
+                <h3>SALDO</h3>
+                <h1 className={styles.element}>R$ 23,00</h1>
+            </div>
+
+            <FaCirclePlus style={{fontSize: '60px', color: '#364248'}} />
         </div>
       </div>
 
       <div className={styles.down}>
-        
         <div className={styles.footer}>
-        
-          <div>
-            <a href="#">
-              <span className={styles.sideIcon}> <MdOutlineRestaurantMenu /> </span>
+        <div>
+            <a href="/#">
+              <MdOutlineRestaurantMenu className={styles.sideIcon} />
             </a>
           </div>
 
           <div className={styles.circle}>
-            <a href="#">
+            <a href="/carteira">
               <span className={styles.plateIcon}> <FaWallet /> </span>
             </a>
           </div>
 
           <div>
             <a href="#">
-              <span className={styles.sideIcon}> <TbMessageExclamation /> </span>
+              <TbMessageExclamation className={styles.sideIcon2}/>
             </a>
           </div>
-
         </div>
-
       </div>
-
     </main>
   )
 }
